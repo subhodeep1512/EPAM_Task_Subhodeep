04a38ecc749ebcad7831512435caf2b1eae8601f
subhodeep1512 <60354065+subhodeep1512@users.noreply.github.com>
2020-07-26 00:38:07 +0530

    Add files via upload

---
+++ DecoratorPattern.java
@@ -0,0 +1,97 @@
+package com.epam.maven_oops;
+package Epam_DesignPattern.ImplementationOfCreationalStructuralBehavioralPatterns.Structural;
+import java.io.BufferedReader;  
+import java.io.IOException;  
+import java.io.InputStreamReader;  
+
+interface Food {  
+    public String prepareFood();  
+    public double foodPrice();  
+}
+class VegFood implements Food {  
+    public String prepareFood(){  
+         return "Veg Food";  
+    }  
+  
+        public double foodPrice(){  
+        return 50.0;  
+    }  
+}  
+abstract class FoodDecorator implements Food{  
+    private Food newFood;  
+    public FoodDecorator(Food newFood)  {  
+        this.newFood=newFood;  
+    }  
+    @Override  
+    public String prepareFood(){  
+        return newFood.prepareFood();   
+    }  
+    public double foodPrice(){  
+        return newFood.foodPrice();  
+    }  
+}  
+class NonVegFood extends FoodDecorator{    
+    public NonVegFood(Food newFood) {  
+        super(newFood);  
+    }  
+    public String prepareFood(){  
+        return super.prepareFood() +" With Roasted Chiken and Chiken Curry  ";   
+    }  
+    public double foodPrice()   {  
+        return super.foodPrice()+150.0;  
+    }  
+}  
+class ChineeseFood extends FoodDecorator{  
+  public ChineeseFood(Food newFood)    {  
+        super(newFood);  
+  }  
+    public String prepareFood(){  
+        return super.prepareFood() +" With Fried Rice and Manchurian  ";   
+    }  
+    public double foodPrice()   {  
+        return super.foodPrice()+65.0;  
+        }  
+}  
+public class DecoratorPattern {  
+    private static int  choice;  
+    public static void main(String args[]) throws NumberFormatException, IOException    {  
+    
+       do{        
+        System.out.print("========= Food Menu ============ \n");  
+        System.out.print("            1. Vegetarian Food.   \n");  
+        System.out.print("            2. Non-Vegetarian Food.\n");  
+        System.out.print("            3. Chineese Food.         \n");  
+        System.out.print("            4. Exit                        \n");  
+        System.out.print("Enter your choice: ");  
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));   
+        choice=Integer.parseInt(br.readLine());  
+        switch (choice) {  
+        case 1:{   
+                 VegFood vf=new VegFood();  
+              System.out.println(vf.prepareFood());  
+              System.out.println( vf.foodPrice());  
+            }  
+            break;  
+              
+       case 2:{  
+                Food f1=new NonVegFood((Food) new VegFood());  
+                    System.out.println(f1.prepareFood());  
+                System.out.println( f1.foodPrice());  
+        }  
+            break;    
+       case 3:{  
+             Food f2=new ChineeseFood((Food) new VegFood());  
+                     System.out.println(f2.prepareFood());  
+                    System.out.println( f2.foodPrice());  
+              }  
+            break;    
+              
+         default:{    
+            System.out.println("Other than these no food available");  
+        }         
+    return;  
+     }
+          
+}while(choice!=4);  
+    }  
+}
---
+++ PrototypePattern.java
@@ -0,0 +1,184 @@
+package com.epam.maven_oops;
+package Epam_DesignPattern.ImplementationOfCreationalStructuralBehavioralPatterns.Creational;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+abstract class Shape {
+    public int x;
+    public int y;
+    public String color;
+
+    public Shape() {
+    }
+
+    public Shape(Shape target) {
+        if (target != null) {
+            this.x = target.x;
+            this.y = target.y;
+            this.color = target.color;
+        }
+    }
+
+    public abstract Shape clone();
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Shape)) return false;
+        Shape shape2 = (Shape) object2;
+        return shape2.x == x && shape2.y == y && Objects.equals(shape2.color, color);
+    }
+}
+class Circle extends Shape {
+    public int radius;
+
+    public Circle() {
+    }
+
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Circle) || !super.equals(object2)) return false;
+        Circle shape2 = (Circle) object2;
+        return shape2.radius == radius;
+    }
+}
+class Rectangle extends Shape {
+    public int width;
+    public int height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(Rectangle target) {
+        super(target);
+        if (target != null) {
+            this.width = target.width;
+            this.height = target.height;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Rectangle) || !super.equals(object2)) return false;
+        Rectangle shape2 = (Rectangle) object2;
+        return shape2.width == width && shape2.height == height;
+    }
+}
+class Demo {
+    public static void main(String[] args) {
+        List<Shape> shapes = new ArrayList<>();
+        List<Shape> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        circle.color = "blue";
+        shapes.add(rectangle);
+
+        cloneAndCompare(shapes, shapesCopy);
+    }
+
+    private static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy) {
+        for (Shape shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+
+        for (int i = 0; i < shapes.size(); i++) {
+            if (shapes.get(i) != shapesCopy.get(i)) {
+                System.out.println(i + ": Shapes are different objects (yay!)");
+                if (shapes.get(i).equals(shapesCopy.get(i))) {
+                    System.out.println(i + ": And they are identical (yay!)");
+                } else {
+                    System.out.println(i + ": But they are not identical (booo!)");
+                }
+            } else {
+                System.out.println(i + ": Shape objects are the same (booo!)");
+            }
+        }
+    }
+}
+class BundledShapeCache {
+    private Map<String, Shape> cache = new HashMap<>();
+
+    public BundledShapeCache() {
+        Circle circle = new Circle();
+        circle.x = 5;
+        circle.y = 7;
+        circle.radius = 45;
+        circle.color = "Green";
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.x = 6;
+        rectangle.y = 9;
+        rectangle.width = 8;
+        rectangle.height = 10;
+        rectangle.color = "Blue";
+
+        cache.put("Big green circle", circle);
+        cache.put("Medium blue rectangle", rectangle);
+    }
+
+    public Shape put(String key, Shape shape) {
+        cache.put(key, shape);
+        return shape;
+    }
+
+    public Shape get(String key) {
+        return cache.get(key).clone();
+    }
+}
+public class PrototypePattern {
+    public static void main(String[] args) {
+        BundledShapeCache cache = new BundledShapeCache();
+
+        Shape shape1 = cache.get("Big green circle");
+        Shape shape2 = cache.get("Medium blue rectangle");
+        Shape shape3 = cache.get("Medium blue rectangle");
+
+        if (shape1 != shape2 && !shape1.equals(shape2)) {
+            System.out.println("Big green circle != Medium blue rectangle (yay!)");
+        } else {
+            System.out.println("Big green circle == Medium blue rectangle (booo!)");
+        }
+
+        if (shape2 != shape3) {
+            System.out.println("Medium blue rectangles are two different objects (yay!)");
+            if (shape2.equals(shape3)) {
+                System.out.println("And they are identical (yay!)");
+            } else {
+                System.out.println("But they are not identical (booo!)");
+            }
+        } else {
+            System.out.println("Rectangle objects are the same (booo!)");
+        }
+    }
+}
